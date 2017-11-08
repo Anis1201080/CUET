dbcdb08ef71d76f252bc3304016d729d7ab36045
Anis <anis.azad47@gmail.com>
2017-11-08 15:33:18 +0600

    start of the project

---
+++ main.js
@@ -0,0 +1,134 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to My Home Page</title>
+    <body style="background-color:powderblue;">
+        <style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+            padding: 15px;
+text-align: left;}
+            <body style="background-color:powderblue;">
+</style>
+
+</head>
+<body>
+    <h1 style="text-align:center;"style="background-color:red;">Md. Anisur Rahman Azad</h1>
+        <img src="Anis&Hillol.JPG" alt="Anis&Hillol" width="1284" height="742">
+    <h1 style="background-color:DodgerBlue;">Hello World</h1>
+ 
+
+  <h1>Heading 1</h1>
+<h2>Heading 2</h2>
+<h3>Heading 3</h3>
+<h4>Heading 4</h4>
+<h5>Heading 5</h5>
+<h6>Heading 6</h6>
+    <b>This text is bold</b> <br>
+    <strong>This text is strong</strong> <br>
+    <i>This text is italic</i> <br>
+    <em>This text is emphasized</em><br> 
+    <h2>HTML <small>Small</small> Formatting</h2>
+    <h2>HTML <mark>Marked</mark> Formatting</h2>
+    <p>My favorite color is <del>blue</del> red.</p>
+    <p>My favorite <ins>color</ins> is red.</p>
+    <p>This is <sub>subscripted</sub> text.</p>
+    <p>This is <sup>superscripted</sup> text.</p>
+<p style="background-color:Tomato;">Lorem ipsum...This paragraph
+contains a lot of lines
+in the source code,
+but the browser 
+ignores it.This paragraph
+contains a lot of lines
+in the source code,
+but the browser 
+ignores it.This paragraph
+contains a lot of lines
+in the source code,
+but the browser 
+ignores it.This paragraph
+contains a lot of lines
+in the source code,
+but the browser 
+ignores it.</p>
+
+
+<p>
+This paragraph
+contains         a lot of spaces
+in the source         code,
+but the        browser 
+ignores it.
+</p> 
+    <a href="form.html">This is a link Reister here</a> <br>
+    <img src="Anis&Hillol.JPG" alt="Anis&Hillol" width="104" height="142"> 
+    <br>
+    <p title="I'm a tooltip">
+This is a paragraph.
+</p>
+    
+
+<h1 style="border:2px solid Tomato;">Hello World</h1>
+<h1 style="border:2px solid DodgerBlue;">Hello World</h1>
+<h1 style="border:2px solid Violet;">Hello World</h1> 
+    <a href="https://www.w3schools.com/html/html_links.asp">This is a url link</a> <br>
+<a href="Picture.jpg">This is Picture Link</a> <br>
+    <a href="https://www.w3schools.com/" target="_blank">Visit W3Schools!</a> 
+    <br>
+  <img src="1.JPG" alt="Md Anisur Rahman Azad" style="width:420px;height:420px;border:50;">
+    
+    
+    <table style="width:100%">
+  <tr>
+    <th>Firstname</th>
+    <th>Lastname</th> 
+    <th>Age</th>
+  </tr>
+  <tr>
+    <td>Jill</td>
+    <td>Smith</td>
+    <td>50</td>
+  </tr>
+  <tr>
+    <td>Eve</td>
+    <td>Jackson</td>
+    <td>94</td>
+  </tr>
+  <tr>
+    <td>John</td>
+    <td>Doe</td>
+    <td>80</td>
+  </tr>
+</table>
+ 
+    <h2>Cell that spans two columns:</h2>
+<table style="width:100%">
+  <tr>
+    <th>Name</th>
+    <th colspan="2">Telephone</th>
+  </tr>
+  <tr>
+    <td>Bill Gates</td>
+    <td>55577854</td>
+    <td>55577855</td>
+  </tr>
+</table>
+
+    <h2>Cell that spans two rows:</h2>
+<table style="width:100%">
+  <tr>
+    <th>Name:</th>
+    <td>Bill Gates</td>
+  </tr>
+  <tr>
+    <th rowspan="2">Telephone:</th>
+    <td>55577854</td>
+  </tr>
+  <tr>
+    <td>55577855</td>
+  </tr>
+</table>
+    
+    </body>
+</html> 
